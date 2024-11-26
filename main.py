8794e29398d42a5986b5a472659b0921847d5eab
Oleksii <oleksii.kompaniiets@outlook.com>
2024-11-26 14:28:43 +0100

    1

--- main.py
+++ main.py
@@ -6,3 +6,21 @@ weight = 60.5  # float
 is_student = True  # bool
 
 print(name, age, weight, is_student)
+print (name)
+
+
+my_list = ["apple", "banana", "cherry"]
+my_list[1] = "blueberry"
+print(my_list)  
+
+prosto= {"name":1, "age": 2}
+print (prosto)
+prosto["email"]= "w@gmail.com"
+print(prosto)
+del prosto["age"]
+print (prosto)
+# Apple
+
+prosto= {"name":1, "age": 2}
+if prosto ["age"] > 1:
+     print ("Apple")
