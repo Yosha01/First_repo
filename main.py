64e61488684dee2799b9d6857210a8a143b077e3
Oleksii <oleksii.kompaniiets@outlook.com>
2024-11-26 14:38:44 +0100

    умови

--- main.py
+++ main.py
@@ -1,26 +1,31 @@
-print ("Hello, Darinka")
+# print ("Hello, Darinka")
 
-name = "Марія"  # str
-age = 25  # int
-weight = 60.5  # float
-is_student = True  # bool
+# name = "Марія"  # str
+# age = 25  # int
+# weight = 60.5  # float
+# is_student = True  # bool
 
-print(name, age, weight, is_student)
-print (name)
+# print(name, age, weight, is_student)
+# print (name)
 
 
-my_list = ["apple", "banana", "cherry"]
-my_list[1] = "blueberry"
-print(my_list)  
+# my_list = ["apple", "banana", "cherry"]
+# my_list[1] = "blueberry"
+# print(my_list)  
+
+# prosto= {"name":1, "age": 2}
+# print (prosto)
+# prosto["email"]= "w@gmail.com"
+# print(prosto)
+# del prosto["age"]
+# print (prosto)
+# # Apple
 
 prosto= {"name":1, "age": 2}
-print (prosto)
-prosto["email"]= "w@gmail.com"
-print(prosto)
-del prosto["age"]
-print (prosto)
-# Apple
-
-prosto= {"name":1, "age": 2}
-if prosto ["age"] > 1:
+if prosto ["age"] >=1:
      print ("Apple")
+
+
+my_dict = {"name": "Олексій", "age": 31}
+if my_dict["age"] >= 18:
+    print("Ви повнолітній")
